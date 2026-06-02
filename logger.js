85f3b81471a776c8eb9e8e5b0f707ff21add458a
Samariddin <samariddinmuhammadqulov001@gmail.com>
2026-06-03 04:05:46 +0500

    fix: middleware path case sensitivity

--- logger.js
+++
@@ -1,8 +0,0 @@
-
-const logger = (req, res, next) => {
-  const time = new Date().toLocaleTimeString('uz-UZ');
-  console.log(`[${time}] ${req.method} ${req.path}`);
-  next();
-};
-
-module.exports = logger;
